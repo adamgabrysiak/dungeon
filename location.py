df014318622cf32f5fbe307ddb9ac6e87669ca1f
Mateusz Zagórski <mateuszzagorski1@gmail.com>
2016-04-06 00:22:10 +0200

    Update location.py

--- location.py
+++ location.py
@@ -9,7 +9,7 @@ def ranloc():
   introdesc = {0 : "a grand hall",
                1 : "a room",}
   middesc = {0 : "which is lit by a single torch. The light gives the promise of safety alas false.",
-             1 : "that resembles a torture chamber. \nVarious tools which sole purpose of existnce is to inflict pain.\nThey are lying on the wooden bench which looks like it was heavliy stained by large amounts of blood.",
+             1 : "that resembles a torture chamber. \nVarious tools which sole purpose of existence is to inflict pain.\nThey are lying on the wooden bench which looks like it was heavliy stained by large amounts of blood.",
              2 : "which you think served as prison. On eastern and western walls there are cells. \nThe iron bars look like you could contract tenatus just by looking at them",
              3 : "that looks like an abandoned canteen. There are traceses of a long fought battle in here. \nLong tables and benches, partly rotten from the humididty that engulfs whole location, overwhelm the room taking all the space."}
   exitdesc = {0 : "north",
