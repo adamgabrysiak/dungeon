9a97445f2d3687e8a6def02f0e775d5a3c633f6f
adamgabrysiak <adamgabrysiak91@gmail.com>
2016-04-06 14:23:12 +0200

    Update location.py

--- location.py
+++ location.py
@@ -1,11 +1,24 @@
 import random
 
+location = []
+i = 1
+
+roomdesc = { 0 : "You are in the wardens office.",
+             1 : "You have found the exit",
+             2 : "Sratatata",
+             3 : "Jeszcze",
+             4 : "nie",
+             5 : "ma",
+             6 : "gotowych",
+             7 : "opisow",
+             8 : "bo nie mam",
+             9 : "pomyslu"}
+
 def ranloc():
   location = []
   intro = random.randrange(2)
   mid = random.randrange(4)
   end = random.randrange(2)
-
   introdesc = {0 : "a grand hall",
                1 : "a room",}
   middesc = {0 : "which is lit by a single torch. The light gives the promise of safety alas false.",
@@ -13,6 +26,15 @@ def ranloc():
              2 : "which you think served as prison. On eastern and western walls there are cells. \nThe iron bars look like you could contract tenatus just by looking at them",
              3 : "that looks like an abandoned canteen. There are traces of a long fought battle in here. \nLong tables and benches, partly rotten from the humididty that engulfs whole location, overwhelm the room taking all the space."}
   exitdesc = {0 : "north",
-            1 : "south"}
+              1 : "south"}
   print("\nYou are in a", introdesc[intro], middesc[mid], "\nYou can exit this location by going", exitdesc[end])
+
+def defloc(roomorder):
+    global roomdesc
+    global i
+    if i <= 10:
+        print(roomdesc[roomorder[i]])
+        i += 1
+    else:
+        ranloc()
 
