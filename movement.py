b14d68b469c6e459c8f8f7ef02e76c5d84958e19
adamgabrysiak <adamgabrysiak91@gmail.com>
2016-05-03 18:04:07 +0200

    Added files via upload

--- movement.py
+++ movement.py
@@ -1,7 +1,6 @@
-import enum
 
-WIDTH = 80
-HEIGHT = 45
+WIDTH = 160
+HEIGHT = 160
 
 def read_state():
 	grid = []
@@ -27,25 +26,34 @@ def read_state():
 		if line.startswith("PLAYER_CARROTS"):
 			carrots = parse(line)[0]
 
-		for field in ["CRATE", "CARROT", "WHITE_RABBIT", "BROWN_RABBIT", "BLACK_RABBIT"]:
+		for bonus in ["CRATE", "CARROT"]:
+			if line.startswith(bonus):
+				x, y = parse(line)
+				grid[x][y] = bonus
+
+		for field in ["WHITE_RABBIT", "BROWN_RABBIT", "BLACK_RABBIT"]:
 			if line.startswith(field):
 				x, y = parse(line)
-				grid[x][y] = field
-				n = 0
-				while n < 5:
-					try:
-						grid[x + n][y] += 1
-						grid[x - n][y] += 1
-						grid[x][y + n] += 1
-						grid[x][y - n] += 1
-						grid[x + n][y + n] += 1
-						grid[x - n][y - n] += 1
-						grid[x - n][y + n] += 1
-						grid[x + n][y - n] += 1
-					except:
-						pass
-					n += 1
-
+				grid[x][y] = 5
+				try:
+					for i in [-5, -4,-3, -2, -1, 0, 1, 2, 3, 4, 5]:
+						for j in [-5, -4,-3, -2, -1, 0, 1, 2, 3, 4, 5]:
+							grid[x + i][y + j] += 1
+					for i in [-4,-3, -2, -1, 0, 1, 2, 3, 4]:
+						for j in [-4,-3, -2, -1, 0, 1, 2, 3, 4]:
+							grid[x + i][y + j] += 1
+					for i in [-3, -2, -1, 0, 1, 2, 3]:
+						for j in [- 3, -2, -1, 0, 1, 2, 3]:
+							grid[x + i][y + j] += 1
+					for i in [ -2, -1, 0, 1, 2]:
+						for j in [-2, -1, 0, 1, 2]:
+							grid[x + i][y + j] += 1
+					for i in [ -1, 0, 1]:
+						for j in [-1, 0, 1]:
+							grid[x + i][y + j] += 1
+				except:
+					pass
+													
 	rabbits = []
 	distances = []
 	
@@ -57,7 +65,7 @@ def read_state():
 				distance = (x - player_x) ** 2 + (y - player_y) ** 2
 				distances.append(distance)
 
-	return grid, rabbits, distances, player_x, player_y
+	return grid, rabbits, distances, player_x, player_y, carrots
 
 def parse(string):
 	array = string[:-1].split("\t")
@@ -72,7 +80,7 @@ def display(grid):
 	for row in grid:
 		print(row)
 
-grid, rabbits, distances, player_x, player_y = read_state()
+grid, rabbits, distances, player_x, player_y, carrots = read_state()
 
 index = 0
 
@@ -94,19 +102,37 @@ LEFT = grid[player_x - 1][player_y]
 RIGHT = grid[player_x + 1][player_y]
 
 player_directions = [UP, DOWN, LEFT, RIGHT]
-print(player_directions)
+
 if sum(player_range_danger) > 0:
-	if player_directions[0] < player_directions[1] and player_directions[0] < player_directions [2] and player_directions[0] < player_directions[3]:
+	if carrots > 0:
+		if player_directions[0] >= player_directions[1] and player_directions[0] >= player_directions [2] and player_directions[0] >= player_directions[3]:
+			print("THROW_UP")
+		elif player_directions[1] >= player_directions[0] and player_directions[1] >= player_directions [2] and player_directions[1] >= player_directions[3]:
+			print("THROW_DOWN")
+		elif player_directions[2] >= player_directions[0] and player_directions[2] >= player_directions [1] and player_directions[2] >= player_directions[3]:
+			print("THROW_LEFT")
+		elif player_directions[3] >= player_directions[0] and player_directions[3] >= player_directions [1] and player_directions[3] >= player_directions[2]:
+			print("THROW_RIGHT")
+	elif player_directions[0] <= player_directions[1] and player_directions[0] <= player_directions [2] and player_directions[0] <= player_directions[3]:
 		print("MOVE_UP")
-	elif player_directions[1] < player_directions[0] and player_directions[1] < player_directions [2] and player_directions[1] < player_directions[3]:
+	elif player_directions[1] <= player_directions[0] and player_directions[1] <= player_directions [2] and player_directions[1] <= player_directions[3]:
 		print("MOVE_DOWN")
-	elif player_directions[2] < player_directions[0] and player_directions[2] < player_directions [1] and player_directions[2] < player_directions[3]:
+	elif player_directions[2] <= player_directions[0] and player_directions[2] <= player_directions [1] and player_directions[2] <= player_directions[3]:
 		print("MOVE_LEFT")
-	elif player_directions[3] < player_directions[0] and player_directions[3] < player_directions [1] and player_directions[3] < player_directions[2]:
+	elif player_directions[3] <= player_directions[0] and player_directions[3] <= player_directions [1] and player_directions[3] <= player_directions[2]:
 		print("MOVE_RIGHT")
 else:
-	pass
+	print("WAIT")
 
 
-
+for columns in range(78):
+	grid[columns][42] += 20
+	grid[columns][41] += 5
+	grid[columns][0] += 20
+	grid[columns][1] += 5
+for rows in range(43):
+ 	grid[77][rows] += 20
+ 	grid[76][rows] += 5
+ 	grid[0][rows] += 20
+ 	grid[1][rows] += 5
 
